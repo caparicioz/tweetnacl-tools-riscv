9b53d3958fcc36872022e6d04698a43a6a3ddfd2
Apa <caparicio0000@hotmail.com>
2018-11-18 15:19:43 -0600

    Histogram added

--- riscv/decrypt_riscv.sh
+++ riscv/decrypt_riscv.sh
@@ -9,8 +9,10 @@ fi
 if [ -d bin ] 
 then
     date
-    spike pk bin/tweetnacl-decrypt sandbox/a.pub sandbox/b.sec sandbox/LordOfTheRings_encrypted.txt sandbox/LordOfTheRings_decrypted.txt 
-    date 
+    spike -g pk bin/tweetnacl-decrypt sandbox/a.pub sandbox/b.sec sandbox/LordOfTheRings_encrypted.txt sandbox/LordOfTheRings_decrypted.txt > tweetnacl-decrypt_LordOfTheRings.out 2> histogram/tweetnacl-decrypt_LordOfTheRings.his
+    date
+    sort -k 2 -n histogram/tweetnacl-decrypt_LordOfTheRings.his > histogram/tweetnacl-decrypt_LordOfTheRings_sort.his
+     
 else
     echo "Run make"
 fi
--- riscv/encrypt_riscv.sh
+++ riscv/encrypt_riscv.sh
@@ -36,11 +36,14 @@ fi
 
 if [ -d bin ] 
 then
+    rm -rf histogram
+    mkdir histogram
     spike pk bin/tweetnacl-keypair sandbox/a.pub sandbox/a.sec
     spike pk bin/tweetnacl-keypair sandbox/b.pub sandbox/b.sec
     date
-    spike pk bin/tweetnacl-encrypt sandbox/a.sec sandbox/b.pub ../input_files/LordOfTheRings.txt sandbox/LordOfTheRings_encrypted.txt
-    date 
+    spike -g pk bin/tweetnacl-encrypt sandbox/a.sec sandbox/b.pub ../input_files/LordOfTheRings.txt sandbox/LordOfTheRings_encrypted.txt > tweetnacl-encrypt_LordOfTheRings.out 2> histogram/tweetnacl-encrypt_LordOfTheRings.his
+    date
+    sort -k 2 -n histogram/tweetnacl-encrypt_LordOfTheRings.his > histogram/tweetnacl-encrypt_LordOfTheRings_sort.his 
 else
     echo "Run make"
 fi
