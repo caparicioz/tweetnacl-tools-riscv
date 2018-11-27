9f4e393cca5491c918da45e9eaef8297c3a49bc6
Aurora <md716924@iteso.mx>
2018-11-26 23:38:50 -0600

    modified SLIL8AND

--- riscv_performance/source/tweetnacl.c
+++ riscv_performance/source/tweetnacl.c
@@ -73,14 +73,14 @@ sv ts64(u8 *x,u64 u)
 
 static int vn(const u8 *x,const u8 *y,int n)
 {
-  u32 i,d = 0;
+  u32 i,d, temp = 0;
   FOR(i,n) d |= x[i]^y[i];
   //return (1 & ((d - 1) >> 8)) - 1;
 asm volatile (
-	"slil8and %[x], %[y], 1 \n" 
-: [x] "=r" (x) 
+	"slil8and %[x], %[y], 8 \n" 
+: [x] "=r" (temp) 
 : [y] "r" (d) );
-return x;
+return temp;
 }
 
 int crypto_verify_16(const u8 *x,const u8 *y)
